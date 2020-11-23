e75edce8af80942baaf454f698099035141aca9e
GuilhermeDumam <guilhermemchitero@gmail.com>
2020-11-23 19:01:29 -0300

    criação do projeto

---
+++ src/main/java/com/rasmoo/cliente/escola/matrizescurriculares/MatrizesCurricularesApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.matrizescurriculares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrizesCurricularesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrizesCurricularesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/cliente/escola/matrizescurriculares/MatrizesCurricularesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.matrizescurriculares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrizesCurricularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
