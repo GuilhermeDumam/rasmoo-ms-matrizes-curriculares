ffd5b410b3de1360b9cbfdb1bf9f177a20cbaf54
GuilhermeDumam <guilhermemchitero@gmail.com>
2020-11-24 14:46:46 -0300

    Criacao da primeira controller

--- src/main/java/com/rasmoo/cliente/escola/matrizescurriculares/MatrizesCurricularesApplication.java
+++ src/main/java/com/rasmoo/cliente/escola/matrizescurriculares/MatrizesCurricularesApplication.java
@@ -2,10 +2,17 @@ package com.rasmoo.cliente.escola.matrizescurriculares;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MatrizesCurricularesApplication {
-
+public class MatrizesCurricularesApplication extends SpringBootServletInitializer  {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MatrizesCurricularesApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MatrizesCurricularesApplication.class, args);
 	}
